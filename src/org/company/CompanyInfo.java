40126f9acaf20dc2895462a8700cf939131906c5
Sarath13-dev <ramsssjrs@gmail.com>
2023-12-28 19:15:21 +0530

    firts

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,34 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+	System.out.println("Zoho");
+
+}
+private void companyId() {
+	System.out.println("234");
+
+}	
+public static void main(String[] args) {
+	CompanyInfo c=new CompanyInfo();
+c.companyName();
+c.companyId();
+
+
+
+
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
